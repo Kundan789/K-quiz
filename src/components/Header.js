9138bf288ceb3d25be479fdbf6af96ca654ffe71
KUNDAN <159764021+Kundan789@users.noreply.github.com>
2025-02-04 18:01:18 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Quiz App 🎮</h1>
+      <p>Test your knowledge and win! 🏆</p>
+    </header>
+  );
+};
+
+export default Header;
